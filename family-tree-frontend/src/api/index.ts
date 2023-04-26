4cb4fabf961b3b54314d5b374f77bc5a1d068392
Rúni Vørmadal <runivormadal@gmail.com>
2023-04-26 20:28:34 +0200

    reduce logging in caprover and correct baseurl for apiclient

--- family-tree-frontend/src/api/index.ts
+++ family-tree-frontend/src/api/index.ts
@@ -1,5 +1,5 @@
 import { ApiClient } from "./ApiClient";
 
-const url = process.env.NODE_ENV === 'production' ? "/" : "https://localhost:5011"
+const url = process.env.NODE_ENV === 'production' ? location.origin : "https://localhost:5011"
 export const FamilyTreeApi = new ApiClient(url)
 
--- scripts/waitForDeploymentCompletion.js
+++ scripts/waitForDeploymentCompletion.js
@@ -47,7 +47,9 @@ const arg = (key) => {
                     throw new Error("could not verify app " + appName);
                 }
 
-                console.log('running...');
+                if (retryCount === 0) {
+                    console.log('running...');
+                }
                 try {
                     const content = stdout.toString().trim();
                     const response = JSON.parse(content.substring(content.indexOf("{")).trim());
@@ -70,11 +72,10 @@ const arg = (key) => {
                         throw new Error(`timeout... current version is ${currentVersion} and latest version is ${latestVersion}`);
                     }
 
-                    if(!appDef.isAppBuilding) {
+                    if (!appDef.isAppBuilding) {
                         throw new Error('Deployment failed. see captain logs for more info');
                     }
                     if (currentVersion !== latestVersion) {
-                        console.log('version do not match', currentVersion, latestVersion);
                         setTimeout(() => check(retryCount + 1), interval);
                     }
 
