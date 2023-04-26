8a80f9d25e48e349fbbfcfe689ef3fc42b192ab6
Rúni Vørmadal <runivormadal@gmail.com>
2023-04-26 20:37:09 +0200

    solve linter error

--- family-tree-frontend/src/api/index.ts
+++ family-tree-frontend/src/api/index.ts
@@ -1,5 +1,5 @@
 import { ApiClient } from "./ApiClient";
 
-const url = process.env.NODE_ENV === 'production' ? location.origin : "https://localhost:5011"
+const url = process.env.NODE_ENV === 'production' ? window.location.origin : "https://localhost:5011"
 export const FamilyTreeApi = new ApiClient(url)
 
